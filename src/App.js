13e7228f40c949c4566a43d54c8fcbd75aaeebdf
Abdulrahman Alsulami <a.sulami96@gmail.com>
2020-01-12 11:33:30 +0300

    bulding the Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Games from './Games';
+import Game from './Game';
+import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <Home></Home>
+        <Contact></Contact>
+        <Games></Games>
+        <Game></Game>
     </div>
   );
 }
---
+++ src/Games.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Games extends Component{
+
+    render(){
+        return(
+            <div>
+                 <p>Test for Games component</p>
+            </div>
+        )
+    }
+}
+
+
+
+export default Games;
